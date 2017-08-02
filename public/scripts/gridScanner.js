b561199699078ba8f8e94c32cbc21a6b50a3fb4b
Timothy Robinson <timmyrobby@gmail.com>
2017-08-02 20:19:09 +0100

    GridScanner can now detect full rows as wins

--- public/scripts/gridScanner.js
+++ public/scripts/gridScanner.js
@@ -7,7 +7,17 @@ define(function() {
   };
 
   GridScanner._scanGrid = function(grid) {
-    return false;
+    return this._findRow(grid);
+  }
+
+  GridScanner._findRow = function(grid) {
+    var result = false;
+    grid.forEach( function(row) {
+      if (row.join('') === 'XXX' || row.join('') === 'OOO') {
+        result = true;
+      }
+    })
+    return result;
   }
 
   return GridScanner;
--- test/gridScannerSpec.js
+++ test/gridScannerSpec.js
@@ -13,13 +13,47 @@ describe('GridScanner', function() {
     // scanner = new GridScanner;
   })
 
-  describe('Initialisation', function() {
+  describe('Win detection', function() {
     it('registers no changes as not game-ending', function() {
       expect(GridScanner.checkForWinner([
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
       ])).equal(false)
+    })
+
+    describe('Rows', function() {
+      it("registers a full row of X's as a win", function() {
+        expect(GridScanner.checkForWinner([
+          ['X', 'X', 'X'],
+          [0, 0, 0],
+          [0, 0, 0]
+        ])).equal(true)
+      })
+
+      it("registers the middle row of X's as a win", function() {
+        expect(GridScanner.checkForWinner([
+          [0, 0, 0],
+          ['X', 'X', 'X'],
+          [0, 0, 0]
+        ])).equal(true)
+      })
+
+      it("registers the bottom row of X's as a win", function() {
+        expect(GridScanner.checkForWinner([
+          [0, 0, 0],
+          [0, 0, 0],
+          ['X', 'X', 'X']
+        ])).equal(true)
+      })
+
+      it("registers a full row of O's as a win", function() {
+        expect(GridScanner.checkForWinner([
+          [0, 0, 0],
+          ['O', 'O', 'O'],
+          [0, 0, 0]
+        ])).equal(true)
+      })
     })
   })
 })
